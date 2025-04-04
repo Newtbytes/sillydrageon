84cfeb2d6dff7297b3f412084536649c8293a13d
Newtbytes <186638664+Newtbytes@users.noreply.github.com>
2025-04-03 19:25:28 -0700

    declare compiler driver pub fn stubs

---
+++ src/driver/lib.rs
@@ -0,0 +1,12 @@
+mod driver;
+
+
+pub fn preprocess(src_fn: String, dst_fn: String) {}
+
+pub fn tokenize(src: String) {}
+
+pub fn parse(tokens: ...) {}
+
+pub fn compile(ast: ...) {}
+
+pub fn assemble(src_fn: String, dst_fn: String) {}
