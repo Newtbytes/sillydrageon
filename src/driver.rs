588493af2671ba5c08e2be1789a820539b88d6e5
Newtbytes <186638664+Newtbytes@users.noreply.github.com>
2025-04-10 16:51:03 -0700

    refactor most of main into driver

--- src/driver.rs
+++ src/driver.rs
@@ -5,6 +5,12 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
+use clap::Parser;
+
+use crate::codegen;
+use crate::error::CompilerError;
+use crate::parser;
+
 const CC: &str = "gcc";
 
 #[derive(Clone, PartialEq)]
@@ -133,4 +139,60 @@ pub fn assemble(src: ProcFile) -> io::Result<ProcFile> {
         .output()?;
 
     Ok(dst)
+}
+
+#[derive(clap::Parser)]
+struct Cli {
+    input: String,
+
+    #[arg(long, action = clap::ArgAction::SetTrue)]
+    lex: bool,
+
+    #[arg(long, action = clap::ArgAction::SetTrue)]
+    parse: bool,
+
+    #[arg(long, action = clap::ArgAction::SetTrue)]
+    codegen: bool,
+}
+
+pub fn run_compiler() -> Result<(), CompilerError> {
+    let cli: Cli = Cli::parse();
+    let input_fn = cli.input;
+
+    let file = ProcFile::from_fn(&input_fn)
+        .ok_or_else(|| CompilerError::ParseError("Invalid source file".to_string()))?;
+
+    let src_file = preprocess(file)?;
+    let asm_file = src_file.to_kind(ProcFileKind::Assembly);
+    let src = src_file.read()?;
+
+    // tokenization
+    let tokens = parser::tokenize(&src)?;
+
+    if cli.lex {
+        dbg!(&tokens);
+        return Ok(());
+    }
+
+    // parsing
+    let ast =
+        parser::parse(&mut tokens.into_iter()).map_err(|msg| CompilerError::ParseError(msg))?;
+
+    if cli.parse {
+        dbg!(ast);
+        return Ok(());
+    }
+
+    // codegen
+    let asm = codegen::lower(&ast);
+    let asm = codegen::emit(&asm);
+    if cli.codegen {
+        println!("{}", asm);
+        return Ok(());
+    }
+
+    asm_file.write(asm)?;
+    assemble(asm_file)?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,12 @@
-use clap::Parser;
-use driver::ProcFileKind;
-use error::CompilerError;
-
 mod codegen;
 mod driver;
 mod error;
 mod parser;
 mod src;
 
-#[derive(clap::Parser)]
-struct Cli {
-    input: String,
-
-    #[arg(long, action = clap::ArgAction::SetTrue)]
-    lex: bool,
-
-    #[arg(long, action = clap::ArgAction::SetTrue)]
-    parse: bool,
-
-    #[arg(long, action = clap::ArgAction::SetTrue)]
-    codegen: bool,
-}
-
-fn main() -> Result<(), CompilerError> {
-    let cli: Cli = Cli::parse();
-    let input_fn = cli.input;
-
-    let file = driver::ProcFile::from_fn(&input_fn)
-        .ok_or_else(|| CompilerError::ParseError("Invalid source file".to_string()))?;
-
-    let src_file = driver::preprocess(file)?;
-    let asm_file = src_file.to_kind(ProcFileKind::Assembly);
-    let src = src_file.read()?;
-
-    // tokenization
-    let tokens = parser::tokenize(&src)?;
-
-    if cli.lex {
-        dbg!(&tokens);
-        return Ok(());
+fn main() {
+    match driver::run_compiler() {
+        Err(e) => eprintln!("{}", e),
+        _ => {}
     }
-
-    // parsing
-    let ast =
-        parser::parse(&mut tokens.into_iter()).map_err(|msg| CompilerError::ParseError(msg))?;
-
-    if cli.parse {
-        dbg!(ast);
-        return Ok(());
-    }
-
-    // codegen
-    let asm = codegen::lower(&ast);
-    let asm = codegen::emit(&asm);
-    if cli.codegen {
-        println!("{}", asm);
-        return Ok(());
-    }
-
-    asm_file.write(asm)?;
-    driver::assemble(asm_file)?;
-
-    Ok(())
 }
