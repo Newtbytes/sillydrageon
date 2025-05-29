dfa41ea14bf87f9b26b4148d50342ca7828a192d
newtbytes <186638664+Newtbytes@users.noreply.github.com>
2025-05-29 15:49:14 -0700

    Revamp Target Emission (#28)
    * Make emission target agnostic
    
    * Implement IR emission target
    
    * Add stub for x86 emission target

--- dialect/src/x86/emit.rs
+++ dialect/src/x86/emit.rs
@@ -1,1 +1,20 @@
+use std::fmt::{self, Formatter};
 
+use lorax::{Block, Context, Emit, EmitTarget, Operation};
+
+pub struct EmitX86 {}
+impl EmitTarget for EmitX86 {
+    type Ctx = Context;
+}
+
+impl Emit<EmitX86> for Operation {
+    fn emit(&self, ctx: &Context, f: &mut Formatter<'_>) -> fmt::Result {
+        todo!("emit x86 assembly for lorax::Operation")
+    }
+}
+
+impl Emit<EmitX86> for Block {
+    fn emit(&self, ctx: &Context, f: &mut Formatter<'_>) -> fmt::Result {
+        todo!("emit x86 assembly for lorax::Block")
+    }
+}
