ba99d944e7bc2eec403ca627096dcf4ff9138962
Newtbytes <186638664+Newtbytes@users.noreply.github.com>
2025-05-30 17:59:47 -0700

    rename block -> bl_ptr/bl

--- dialect/src/x86/from_func.rs
+++ dialect/src/x86/from_func.rs
@@ -2,14 +2,14 @@ use lorax::{Context, Ptr};
 
 use super::{ops::*, state::ax};
 
-pub fn lower_func(ctx: &mut Context, block: Ptr, op_ptr: Ptr) {
-    let block = ctx.blocks.deref_mut(block);
+pub fn lower_func(ctx: &mut Context, bl_ptr: Ptr, op_ptr: Ptr) {
+    let bl = ctx.blocks.deref_mut(bl_ptr);
     let op = ctx.ops.deref(op_ptr);
 
     if let ("func.ret", &[val]) = (op.name, op.operands.as_slice()) {
-        let v0 = block.insert_behind(&mut ctx.ops, op_ptr, ax());
-        let _ = block.insert_behind(&mut ctx.ops, op_ptr, mov(val, v0));
+        let v0 = bl.insert_behind(&mut ctx.ops, op_ptr, ax());
+        let _ = bl.insert_behind(&mut ctx.ops, op_ptr, mov(val, v0));
 
-        block.replace(&mut ctx.ops, op_ptr, ret());
+        bl.replace(&mut ctx.ops, op_ptr, ret());
     }
 }
